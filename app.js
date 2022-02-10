6cb806d62aeba1a9914b8db1300f6c9261871c29
João Magalhães <joamag@gmail.com>
2022-02-10 00:48:26 +0000

    feat: new default error handler

--- app.js
+++ app.js
@@ -41,6 +41,24 @@ app.get("/info", (req, res, next) => {
     });
 });
 
+app.all("*", (req, res, next) => {
+    res.status(404);
+    res.json({ error: "Route not found", code: 404 });
+});
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+    const code = err.code || 500;
+    const result = { error: err.message, code: code };
+    if (process.env.NODE_ENV !== "production") {
+        result.stack = err.stack ? err.stack.split("\n") : [];
+    }
+    res.status(code);
+    res.json(result);
+});
+
 (async () => {
     await lib.start();
     try {
